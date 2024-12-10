3846cfcde7d6ef25ee1287655b9b5715ba8f1acd
keninoue30 <rokinonntyu.rad@gmail.com>
2024-12-10 21:33:39 +0900

    1210

--- src/SetSample.java
+++ src/SetSample.java
@@ -8,14 +8,16 @@ public class SetSample {
 
 	Set<String>set = new HashSet<String>();
 
+		//値のセットはaddメソッドを利用
 		set.add("value1");
 		set.add("value2");
 		set.add("value3");
 		set.add("value4");
 		set.add("value5");
 
-		set.add("value2");
+		set.add("value2"); //重複要素をadd
 
+		//拡張for文を利用
 		for(String s : set){
 			System.out.println(s);
 		}
